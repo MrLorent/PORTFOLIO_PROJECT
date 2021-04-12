1a38b0563d86b6120cd16b20d8cbb7f1c7fc8a88
T_Lorent <lorent.tanguy@gmail.com>
2021-04-12 15:02:08 +0200

    00-Setup_de_base_2.1

---
+++ js/publicRequests.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// EXEMPLE :
+// async function getPlanetDetails(planetId){
+//     const response = await fetch('./SERVER/router.php/planet/'+planetId);
+//     const planets = await response.json();
+    
+//     console.log(planets);
+//     return planets;
+// }
--- php/categoriesRouter.php
+++ php/categoriesRouter.php
@@ -3,3 +3,53 @@
 require_once('categoriesControllers.php');
 
 // ROUTEUR
+
+$request = explode('/', $_SERVER['REQUEST_URI']);
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch($request[3]){
+    case /* endpoint name */:
+        switch($method){
+            case 'GET':
+                
+                break;
+            case 'POST':
+                
+                break;
+            case 'PUT':
+                
+                break;
+            case 'DELETE':
+
+                break;
+            default:
+                http_response_code('404');
+                echo 'OUPSI !';
+                break;
+        }
+        break;
+    case /* endpoint name */:
+        switch($method){
+            case 'GET':
+                
+                break;
+            case 'POST':
+                
+                break;
+            case 'PUT':
+                
+                break;
+            case 'DELETE':
+
+                break;
+            default:
+                http_response_code('404');
+                echo 'OUPSI !';
+                break;
+        }
+        break;
+    default :
+        http_response_code('500');
+        echo 'unknown endpoint';
+        break;
+}
