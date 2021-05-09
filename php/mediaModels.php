cc3e011bdbac1582edf36b1632c2d0c64aefa3e0
Marie Guillot <guillot-marie@hotmail.fr>
2021-05-09 15:45:54 +0200

    add Media mais on ne peut pas tester

--- php/mediaModels.php
+++ php/mediaModels.php
@@ -9,3 +9,23 @@ function getProjectMedia($idProject) {
     $mediaProject = $rqt->fetchAll(PDO::FETCH_ASSOC);
     return $mediaProject;
 }
+
+function addMedium($typefichier) {
+       // creation du medium
+       $cnx = connection();
+       $rqt = $cnx->prepare('INSERT INTO media VALUES( NULL, ?, ?, ?, ?)');
+       $rqt->execute(array('chemin temporaire', $_POST['legende'], $typefichier, $_POST['idProjet']));
+
+       //recuperation de l'id du medium
+       $rqt = $cnx->prepare('SELECT `idMedia` FROM `media` WHERE `idProjet`=? AND `source`="chemin temporaire"');
+       $rqt->execute($_POST['idProjet']);
+       $idMedium = $rqt->fetch();
+
+       //insertion de l'image dans les dossiers
+       move_uploaded_file($_FILES['medium']['tmp_name'], './img/gallery/'.$_POST['idProjet']."/".$idMedium);
+       $cheminfichier = './img/gallery/'.$_POST['idProjet']."/".$idMedium;
+
+       //correction de la bdd
+       $rqt = $cnx->prepare( 'UPDATE media SET source = ?, WHERE idMedia = ?');
+       $rqt->execute(array($cheminfichier, $idMedium));
+}
